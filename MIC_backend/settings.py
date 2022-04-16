076633a8e86942d60b3b74fc2fbc5cdad745fbe2
Susindhar Arumugapriya Venkatesan <susindhar02@gmail.com>
2022-04-16 21:00:20 +0530

    Added new signup page and CSS

--- MIC_backend/settings.py
+++ MIC_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
 
 # Application definition
 
